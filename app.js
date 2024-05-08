ead90c3c4959d982e780dbcc076cc9ba4f33d6df
Ali Elawady <eali83051@gmail.com>
2024-05-08 18:13:41 +0300

    First Commit

---
+++ app.js
@@ -0,0 +1,59 @@
+const express = require("express")
+const cors = require("cors")
+const app = express();
+app.use(cors());
+app.use(express.json());
+const Port = 4000 ; 
+
+
+app.get("/" , async(Req , Res) => {
+
+let Players = {
+L0 : [
+["Ali Ahmed" , "علي أحمد"],
+["Kareem Ayman" , "كريم أيمن"],
+["Youssef Hussien" , "يوسف حسين"],
+],
+L1 : [
+    ["Ali Elawady" , "علي العوضي"],
+    ["Mahmoud Elawady" , "محمود العوضي"],
+    ["Ahmed Islam" , "أحمد إسلام"],
+    ["Mostafa Emad" , "مصطفى عماد"],
+    ["Youssef Jawish" , "يوسف جاويش"],
+    ["Mohammed Alaa" , "محمد علاء"],
+    ["Zeyad Emad" , "زياد عماد"],
+    ["Kareem Eldeeb" , "كريم الديب"],
+    ["Mohammed Osama" , "محمد أسامة"],
+    ["Ahmed Mohammed" , "أحمد محمد"],
+    ["Youssef Hegazy" , "يوسف حجازي"],
+
+],
+L2 : [
+    ["Ashraf Elawady" , "أشرف العوضي"],
+    ["Yassin Emad" , "ياسين عماد"],
+    ["Ahmed Emad" , "أحمد عماد"],
+    ["Mohammed Ismail" , "محمد إسماعيل"],
+
+
+]
+} 
+
+
+
+    Res.json(Players)
+})
+
+
+
+
+
+
+
+
+
+
+//App lisening
+
+app.listen(Port, () =>{
+    console.log(`Server is started on port ${Port} , The Link: http://localhost:${Port}`)
+})
